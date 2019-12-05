2d704b8e40cb2da32d669e705935acd6d685a16d
pl <plrtn@protonmail.com>
2019-12-05 17:26:04 -0500

    completed prime and median computation functions

--- src/helpers/math.ts
+++ src/helpers/math.ts
@@ -1,15 +1,44 @@
+import { array } from "prop-types"
+
 const getSequence = (n:number) => {
+  // exception guards
+  if (n<0) return []
+
   const [zero, one, ...rest] = [...Array(n).keys()]
   return rest
 }
 
 const getPrimes = (ns : number[]) => {
   const primes : number[] = []
-  const limit : number = Math.sqrt(ns.length + 2)
 
-  for (let counter = 2; counter <= limit; counter++){
+  // Do not need to check past limit because every number past it is guaranteed to be a prime #
+  const limit : number = Math.ceil(Math.sqrt(ns.length + 2))
+
+  for (let counter = ns[0]; counter <= limit; counter++){
       primes.push(ns[0])
-      ns = ns.filter(v => v%counter !== 0)
+      ns = ns.filter(v => v%ns[0]!== 0)
   }
   return [...primes, ...ns]
+}
+
+const getMedian = (ns: number[]) => {
+  const len = ns.length
+  if (len > 0){
+    if (len % 2 === 0){
+      return [ns[len/2-1], ns[len/2]]
+    }
+    else {
+      return [ns[Math.floor(len/2)]]
+    }
+  }
+  return []
+}
+
+const getResult = (n: number) => getMedian(getPrimes(getSequence(n))) 
+
+export {
+  getSequence,
+  getPrimes,
+  getMedian,
+  getResult,
 }
