0d23ed310c14724b0a234586faf86d5e7911b138
pl <plrtn@protonmail.com>
2019-12-08 11:46:14 -0500

    installed and init'd  prod middleware compression, helmet

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from 'express'
+import  compression from 'compression'
+import  helmet  from 'helmet'
+//
 import { router } from './routes/index'
 
 const app = express()
 const port = process.env.PORT || 8080
 
+//middleware init
+app.use(compression())
+app.use(helmet())
 app.use(express.json())
+
+//routes
 app.use('/', router)
 
 // 404 catchall
