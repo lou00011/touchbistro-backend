492d34dfc04408fb3e1a59d5442e9c831e6e9525
pl <plrtn@protonmail.com>
2019-12-09 22:53:47 -0500

    Added cors middleware and dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import  compression from 'compression'
 import  helmet  from 'helmet'
+import cors from 'cors'
 //
 import { router } from './routes/index'
 
@@ -10,6 +11,9 @@ const port = process.env.PORT || 8080
 //middleware init
 app.use(compression())
 app.use(helmet())
+app.use(cors())
+
+// Note, restrict cors on my end point after its hosted.
 app.use(express.json())
 
 //routes
